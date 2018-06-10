46b2af4136aca65c167a3b6db1e54ace2b2f2f30
Vattanan <vattanan.bu@gmail.com>
2018-06-10 17:35:09 +0700

    refactoring

--- programming/Person.js
+++ programming/Person.js
@@ -1,15 +1,17 @@
 const initialCurrentID = 0;
 class Person {
-  constructor(name, fiendes = []) {
+  constructor(name, friends = []) {
     // validation name parameter
     if (!name || typeof name !== 'string' || name.trim() === '') {
       throw new Error('name not correct format');
     }
 
+    // if (friends.every(friend => friend))
+
     Person.currentMaxID = Person.currentMaxID + 1;
     this.id = Person.currentMaxID;
     this.name = name;
-    this.fiendes = [];
+    this.friends = [];
   }
 }
 
--- programming/__tests__/Person.test.js
+++ programming/__tests__/Person.test.js
@@ -2,7 +2,7 @@ import Person, { initialCurrentID } from '../Person';
 
 const defaultParamsPerson = {
   name: 'Person Test',
-  fiendes: [],
+  friends: [],
 };
 
 describe('Person class', () => {
@@ -11,58 +11,58 @@ describe('Person class', () => {
   });
 
   it('Can create new instance from Person', () => {
-    const person = new Person(defaultParamsPerson.name, defaultParamsPerson.fiendes); 
+    const person = new Person(defaultParamsPerson.name, defaultParamsPerson.friends); 
     expect(person).toBeInstanceOf(Person);
   });
 
   it('when sent name not correct to Person must throw error', () => {
-    const { fiendes } = defaultParamsPerson;
+    const { friends } = defaultParamsPerson;
     // name is undefined
     expect(() => {
-      new Person(undefined, fiendes);
+      new Person(undefined, friends);
     }).toThrowError('name not correct format');
     // name is ''
     expect(() => {
-      new Person('', fiendes);
+      new Person('', friends);
     }).toThrowError('name not correct format');
     // name is number
     expect(() => {
-      new Person(63, fiendes);
+      new Person(63, friends);
     }).toThrowError('name not correct format');
     // name is space
     expect(() => {
-      new Person('            ', fiendes)
+      new Person('            ', friends)
     }).toThrowError('name not correct format');
   });
 
-  it('when not sent fiendes to Person, person instance must have fiendes = []', () => {
+  it('when not sent friends to Person, person instance must have friends = []', () => {
     const person = new Person(defaultParamsPerson.name);
-    expect(person.fiendes).toBeDefined();
-    expect(Array.isArray(person.fiendes)).toBeTruthy();
-    expect(person.fiendes).toHaveLength(0);
+    expect(person.friends).toBeDefined();
+    expect(Array.isArray(person.friends)).toBeTruthy();
+    expect(person.friends).toHaveLength(0);
   });
 
-  it('if fiendes elements is not instance of Person, then Person must throw error', () => {    
+  it('if friends elements is not instance of Person, then Person must throw error', () => {    
     expect(() => {
       new Person(defaultParamsPerson.name, ['penson1', 'penson2', 'penson3']);
-    }).toThrowError('element of fiendes must be instance of Person');
+    }).toThrowError('element of friends must be instance of Person');
   });
 
-  it('when sent name and fiendes to Person, person instance must have name and fiendes is by parameter', () => {
-    const { name, fiendes } = defaultParamsPerson;
-    const person = new Person(name, fiendes);
+  it('when sent name and friends to Person, person instance must have name and friends is by parameter', () => {
+    const { name, friends } = defaultParamsPerson;
+    const person = new Person(name, friends);
     expect(person.name).toBe(name);
-    expect(person.fiendes).toEqual(fiendes);
+    expect(person.friends).toEqual(friends);
   });  
 
-  it('Person instance can set name and fiendes', () => {
-    const person = new Person(defaultParamsPerson.name, defaultParamsPerson.fiendes);
+  it('Person instance can set name and friends', () => {
+    const person = new Person(defaultParamsPerson.name, defaultParamsPerson.friends);
     // name
     expect(person.name).toBeDefined();
     expect(typeof person.name).toBe('string');
-    // fiendes
-    expect(person.fiendes).toBeDefined();
-    expect(Array.isArray(person.fiendes)).toBeTruthy();
+    // friends
+    expect(person.friends).toBeDefined();
+    expect(Array.isArray(person.friends)).toBeTruthy();
   });
 
   it('Person must have current max id and it\'s integer', () => {
@@ -73,35 +73,35 @@ describe('Person class', () => {
   it('current max id start at initialtMaxID and must increase when new instance n + 1', () => {
     expect(Person.currentMaxID).toBe(initialCurrentID);
     // person 1
-    new Person(defaultParamsPerson.name, defaultParamsPerson.fiendes);
+    new Person(defaultParamsPerson.name, defaultParamsPerson.friends);
     expect(Person.currentMaxID).toBe(initialCurrentID + 1);
     // person 2
-    new Person(defaultParamsPerson.name, defaultParamsPerson.fiendes);
+    new Person(defaultParamsPerson.name, defaultParamsPerson.friends);
     expect(Person.currentMaxID).toBe(initialCurrentID + 2);
   });
 
   it('Person instance must have id, id must be integer', () => {
-    const person = new Person(defaultParamsPerson.name, defaultParamsPerson.fiendes);
+    const person = new Person(defaultParamsPerson.name, defaultParamsPerson.friends);
     expect(person.id).toBeDefined();
     expect(Number.isInteger(person.id)).toBeDefined();
   });
 
   it('id of Person must equal than current current max id when genarate', () => {
     // person 1
-    const person1 = new Person(defaultParamsPerson.name, defaultParamsPerson.fiendes);
+    const person1 = new Person(defaultParamsPerson.name, defaultParamsPerson.friends);
     expect(person1.id).toBe(Person.currentMaxID);
     // person 2
-    const person2 = new Person(defaultParamsPerson.name, defaultParamsPerson.fiendes);
+    const person2 = new Person(defaultParamsPerson.name, defaultParamsPerson.friends);
     expect(person2.id).toBe(Person.currentMaxID);
     // person 3
-    const person3 = new Person(defaultParamsPerson.name, defaultParamsPerson.fiendes);
+    const person3 = new Person(defaultParamsPerson.name, defaultParamsPerson.friends);
     expect(person3.id).toBe(Person.currentMaxID);
   });
 
   it('id each of Person instance must unique', () => {
-    const person1 = new Person(defaultParamsPerson.name, defaultParamsPerson.fiendes);
-    const person2 = new Person(defaultParamsPerson.name, defaultParamsPerson.fiendes);
-    const person3 = new Person(defaultParamsPerson.name, defaultParamsPerson.fiendes);
+    const person1 = new Person(defaultParamsPerson.name, defaultParamsPerson.friends);
+    const person2 = new Person(defaultParamsPerson.name, defaultParamsPerson.friends);
+    const person3 = new Person(defaultParamsPerson.name, defaultParamsPerson.friends);
     expect(person1.id).not.toBe(person2.id);
     expect(person1.id).not.toBe(person3.id);
     expect(person2.id).not.toBe(person3.id);
