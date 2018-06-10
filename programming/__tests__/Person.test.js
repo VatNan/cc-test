318cb5fc58028d03812179c107aeb9ec7c59e74e
Vattanan <vattanan.bu@gmail.com>
2018-06-10 15:12:55 +0700

    add test 'Person must have max id and it\'s integer'

--- programming/__tests__/Person.test.js
+++ programming/__tests__/Person.test.js
@@ -1,8 +1,14 @@
 import Person from '../Person';
 
 describe('Person class', () => {
-  it('Person can create new instance', () => {
+  it('Can create new instance from Person', () => {
     const person = new Person(); 
     expect(person).toBeInstanceOf(Person);
   });
+
+  it('Person must have max id and it\'s integer', () => {
+    expect(Person.maxID).toBeDefined();
+    expect(Number.isInteger(Person.maxID)).toBeTruthy();
+  });
+
 });
